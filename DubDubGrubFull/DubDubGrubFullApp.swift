b30071a8901549c9d31ddedf394a829ae757fe3f
Jakhangir <Jakhangir799@gmail.com>
2023-01-30 14:33:53 +0000

    Creating just project

---
+++ DubDubGrubFull/DubDubGrubFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubFullApp.swift
+//  DubDubGrubFull
+//
+//  Created by Jahongir on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
